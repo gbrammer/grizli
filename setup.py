19496873623c9fdb99b8fbd57965e308ddb39f0f
Gabriel Brammer <brammer@stsci.edu>
2016-04-22 14:43:52 -0400

    add cythonized c files

--- setup.py
+++ setup.py
@@ -1,21 +1,31 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
+try:
+    from Cython.Build import cythonize
+    USE_CYTHON = True
+    cext = '.pyx'
+except ImportError:
+    USE_CYTHON = False
+    cext = '.c'
+    
 import os
 import numpy
 
 extensions = [
-    Extension("grizli/utils_c/interp", ["grizli/utils_c/interp.pyx"],
+    Extension("grizli/utils_c/interp", ["grizli/utils_c/interp"+cext],
         include_dirs = [numpy.get_include()],),
         
-    # Extension("grizli/utils_c/nmf", ["grizli/utils_c/nmf.pyx"],
+    # Extension("grizli/utils_c/nmf", ["grizli/utils_c/nmf"+cext],
     #     include_dirs = [numpy.get_include()],),
     
-    Extension("grizli/utils_c/disperse", ["grizli/utils_c/disperse.pyx"],
+    Extension("grizli/utils_c/disperse", ["grizli/utils_c/disperse"+cext],
         include_dirs = [numpy.get_include()],),
-    
+
 ]
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -26,13 +36,13 @@ def read(fname):
 
 setup(
     name = "grizli",
-    version = "0.1.4",
+    version = "0.1.0",
     author = "Gabriel Brammer",
     author_email = "gbrammer@gmail.com",
     description = "Grism redshift and line analysis software",
     license = "MIT",
     url = "https://github.com/gbrammer/grizli",
-    download_url = "https://github.com/gbrammer/grizli/tarball/0.1.4",
+    download_url = "https://github.com/gbrammer/grizli/tarball/0.1.0",
     packages=['grizli', 'grizli/utils_c'],
     # requires=['numpy', 'scipy', 'astropy', 'drizzlepac', 'stwcs'],
     # long_description=read('README.rst'),
@@ -41,7 +51,7 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Astronomy',
     ],
-    ext_modules = cythonize(extensions),
+    ext_modules = extensions,
     package_data={'grizli': ['data/*']},
     # scripts=['grizli/scripts/flt_info.sh'],
 )
