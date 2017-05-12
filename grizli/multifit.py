a69c825159db846a34ddcd45dfbb9f736d7d31b7
Gabriel Brammer <brammer@stsci.edu>
2017-05-12 16:53:37 -0400

    First verion of dual-mode fitting in GroupFitter

--- grizli/multifit.py
+++ grizli/multifit.py
@@ -901,7 +901,347 @@ class GroupFitter(object):
             A_bg[i, self.slices[i]] = 1.
         
         return A_bg
+    
+    def xfit_at_z(self, z=0, templates=[], fitter='nnls', fit_background=True, get_uncertainties=False):
+        """Fit the 2D spectra with a set of templates at a specified redshift.
         
+        Parameters
+        ----------
+        z : float
+            Redshift.
+        
+        templates : list
+            List of templates to fit.
+        
+        fitter : str
+            Minimization algorithm to compute template coefficients.
+            The default 'nnls' uses non-negative least squares.  
+            The other option is standard 'leastsq'.
+        
+        fit_background : bool
+            Fit additive pedestal background offset.
+            
+        get_uncertainties : bool
+            Compute coefficient uncertainties from the covariance matrix
+        
+        
+        Returns
+        -------
+        chi2 : float
+            Chi-squared of the fit
+        
+        background : `~np.ndarray`
+            Background model
+        
+        model2d : `~np.ndarray`
+            Best-fit 2D model.
+        
+        coeffs, coeffs_err : `~np.ndarray`
+            Template coefficients and uncertainties.
+        
+        """
+        import scipy.optimize
+        
+        NTEMP = len(templates)
+        A = np.zeros((self.N+NTEMP, self.Ntot))
+        A[:self.N,:] += self.A_bg*fit_background
+                        
+        for i, t in enumerate(templates):
+            ti = templates[t]
+            try:
+                import eazy.igm
+                if z > 5:
+                    igm = eazy.igm.Inoue14()
+                    igmz = igm.full_IGM(z, ti.wave*(1+z))         
+                else:
+                    igmz = 1.
+            except:
+                igmz = 1.
+
+            s = [ti.wave*(1+z), ti.flux/(1+z)*igmz]
+            
+            for j, beam in enumerate(self.beams):
+                mask_i = beam.fit_mask.reshape(beam.sh)
+                clip = mask_i.sum(axis=0) > 0        
+                if clip.sum() == 0:
+                    continue
+                                
+                lam_beam = beam.wave[clip]
+                if ((s[0].min() > lam_beam.max()) | 
+                    (s[0].max() < lam_beam.min())):
+                    continue
+
+                sl = self.slices[j]
+                A[self.N+i, sl] = beam.compute_model(spectrum_1d=s, in_place=False, is_cgs=True)
+                    
+        oktemp = (A*self.fit_mask).sum(axis=1) != 0
+        
+        if fit_background:
+            if fitter == 'nnls':
+                pedestal = 0.04
+            else:
+                pedestal = 0.
+        else:
+            pedestal = 0
+            
+        # Weight design matrix and data by 1/sigma
+        Ax = A[oktemp,:]*self.sivarf        
+        AxT = Ax[:,self.fit_mask].T
+        data = ((self.scif+pedestal)*self.sivarf)[self.fit_mask]
+        
+        # Run the minimization
+        if fitter == 'nnls':
+            coeffs_i, rnorm = scipy.optimize.nnls(AxT, data)            
+        else:
+            coeffs_i, residuals, rank, s = np.linalg.lstsq(AxT, data)
+       
+        # Compute background array         
+        if fit_background:
+            background = np.dot(coeffs_i[:self.N], A[:self.N,:]) - pedestal
+            coeffs_i[:self.N] -= pedestal
+        else:
+            background = self.scif*0.
+        
+        # Full model
+        model2d = np.dot(coeffs_i[self.N:], Ax[self.N:,:]/self.sivarf)
+        
+        # Residuals and Chi-squared
+        resid = self.scif - model2d - background
+        chi2 = np.sum(resid[self.fit_mask]**2*self.sivarf[self.fit_mask]**2)
+        
+        # Uncertainties from covariance matrix
+        if get_uncertainties:
+            try:
+                # Covariance is inverse of AT.A
+                covar_i = np.matrix(np.dot(AxT.T, AxT)).I.A
+                covar = utils.fill_masked_covar(covar_i, oktemp)
+                covard = np.sqrt(covar.diagonal())
+            except:
+                print('Except: covar!')
+                covar = np.zeros((self.N+NTEMP, self.N+NTEMP))
+                covard = np.zeros(self.N+NTEMP)#-1.
+        else:
+            covar = np.zeros((self.N+NTEMP, self.N+NTEMP))
+            covard = np.zeros(self.N+NTEMP)#-1.
+        
+        coeffs = np.zeros(self.N+NTEMP)
+        coeffs[oktemp] = coeffs_i #[self.N:]] = coeffs[self.N:]
+
+        coeffs_err = covard #np.zeros(NTEMP)
+        #full_coeffs_err[oktemp[self.N:]] = covard[self.N:]
+        
+        return chi2, background, model2d, coeffs, coeffs_err, covar
+    
+    def xfit_redshift(self, prior=None, fwhm=1200,
+                     make_figure=True, zr=None, dz=None, verbose=True,
+                     fit_background=True, fitter='nnls', 
+                     delta_chi2_threshold=0.004, zoom=True, 
+                     line_complexes=True, templates={}, figsize=[8,5],
+                     fsps_templates=False):
+        """TBD
+        """
+        from scipy import polyfit, polyval
+        
+        if zr is None:
+            zr = [0.65, 1.6]
+        
+        if dz is None:
+            dz = [0.005, 0.0004]
+        
+        if zr is 0:
+            stars = True
+            zr = [0, 0.01]
+            fitter='nnls'
+        else:
+            stars = False
+            
+        zgrid = utils.log_zgrid(zr, dz=dz[0])
+        NZ = len(zgrid)
+        
+        #### Polynomial SED fit
+        wpoly = np.arange(1000,5.e4,1000)
+        tpoly = utils.polynomial_templates(wpoly, line=True)
+        out = self.xfit_at_z(z=0., templates=tpoly, fitter='nnls',
+                            fit_background=True)
+        #
+        tpoly = utils.polynomial_templates(wpoly, order=3)
+        out = self.xfit_at_z(z=0., templates=tpoly, fitter='lstsq',
+                            fit_background=True)
+                            
+        chi2_poly, b, m2, coeffs_poly, c, cov = out
+        if True:
+            cp, lp = utils.dot_templates(coeffs_poly[self.N:], tpoly)
+            
+        ### Set up for template fit
+        if templates == {}:
+            templates = utils.load_templates(fwhm=fwhm, stars=stars, line_complexes=line_complexes, fsps_templates=fsps_templates)
+        else:
+            if verbose:
+                print('User templates! N={0} \n'.format(len(templates)))
+            
+        NTEMP = len(templates)
+        
+        out = self.xfit_at_z(z=0., templates=templates, fitter=fitter,
+                            fit_background=fit_background)
+                            
+        chi2, background, model2d, coeffs, coeffs_err, covar = out
+        
+        chi2 = np.zeros(NZ)
+        coeffs = np.zeros((NZ, coeffs.shape[0]))
+        
+        chi2min = 1e30
+        iz = 0
+        for i in range(NZ):
+            out = self.xfit_at_z(z=zgrid[i], templates=templates,
+                                fitter=fitter, fit_background=fit_background)
+            
+            chi2[i], background, model2d, coeffs[i,:], coeffs_err, covar = out
+            if chi2[i] < chi2min:
+                iz = i
+                chi2min = chi2[i]
+
+            if verbose:                    
+                print(utils.NO_NEWLINE + '  {0:.4f} {1:9.1f} ({2:.4f})'.format(zgrid[i], chi2[i], zgrid[iz]))
+        
+        print('First iteration: z_best={0:.4f}\n'.format(zgrid[iz]))
+            
+        ## Find peaks
+        import peakutils
+                
+        chi2_rev = (chi2_poly - chi2)/self.DoF
+        if chi2_poly < (chi2.min() + 9):
+            chi2_rev = (chi2.min() + 16 - chi2)/self.DoF
+
+        chi2_rev[chi2_rev < 0] = 0
+        indexes = peakutils.indexes(chi2_rev, thres=0.4, min_dist=8)
+        num_peaks = len(indexes)
+        
+        if False:
+            plt.plot(zgrid, (chi2-chi2.min())/ self.DoF)
+            plt.scatter(zgrid[indexes], (chi2-chi2.min())[indexes]/ self.DoF, color='r')
+        
+        # delta_chi2 = (chi2.max()-chi2.min())/self.DoF
+        # if delta_chi2 > delta_chi2_threshold:      
+        if (num_peaks > 0) & (not stars) & zoom:
+            zgrid_zoom = []
+            for ix in indexes:
+                if (ix > 0) & (ix < len(chi2)-1):
+                    c = polyfit(zgrid[ix-1:ix+2], chi2[ix-1:ix+2], 2)
+                    zi = -c[1]/(2*c[0])
+                    chi_i = polyval(c, zi)
+                    zgrid_zoom.extend(np.arange(zi-2*dz[0], 
+                                      zi+2*dz[0]+dz[1]/10., dz[1]))
+                    
+            # zgrid_zoom = utils.zoom_zgrid(zgrid, chi2/self.DoF,
+            #                               threshold=delta_chi2_threshold,
+            #                               factor=dz[0]/dz[1])
+            NZOOM = len(zgrid_zoom)
+        
+            chi2_zoom = np.zeros(NZOOM)
+            coeffs_zoom = np.zeros((NZOOM, coeffs.shape[1]))
+
+            iz = 0
+            chi2min = 1.e30
+            for i in range(NZOOM):
+                out = self.xfit_at_z(z=zgrid_zoom[i], templates=templates,
+                                    fitter=fitter,
+                                    fit_background=fit_background)
+
+                chi2_zoom[i], b, m2, coeffs_zoom[i,:], err, cov = out
+                #A, coeffs_zoom[i,:], chi2_zoom[i], model_2d = out
+                if chi2_zoom[i] < chi2min:
+                    chi2min = chi2_zoom[i]
+                    iz = i
+                
+                if verbose:
+                    print(utils.NO_NEWLINE+'- {0:.4f} {1:9.1f} ({2:.4f}) {3:d}/{4:d}'.format(zgrid_zoom[i], chi2_zoom[i], zgrid_zoom[iz], i+1, NZOOM))
+        
+            zgrid = np.append(zgrid, zgrid_zoom)
+            chi2 = np.append(chi2, chi2_zoom)
+            coeffs = np.append(coeffs, coeffs_zoom, axis=0)
+        
+        so = np.argsort(zgrid)
+        zgrid = zgrid[so]
+        chi2 = chi2[so]
+        coeffs=coeffs[so,:]
+        
+        return zgrid, chi2, coeffs, chi2_poly
+        
+    def template_at_z(self, z=0, templates=None, fit_background=True):
+        """TBD
+        """
+        if templates is None:
+            templates = utils.load_templates(line_complexes=False, fsps_templates=True, fwhm=1400)
+        
+        out = self.xfit_at_z(z=z, templates=templates, fitter='nnls', 
+                             fit_background=fit_background,
+                             get_uncertainties=True)
+
+        chi2, modelBG, model2D, coeffs, coeffs_err, covar = out
+        cont1d, line1d = utils.dot_templates(coeffs[self.N:], templates, z=z)
+            
+    def process_zfit(self, zgrid, chi2):
+        """Parse redshift fit"""
+        
+        zbest = zgrid[np.argmin(chi2)]
+        
+        ###############
+        
+        if prior is not None:
+            #print('\n\nPrior!\n\n', chi2.min(), prior[1].min())
+            interp_prior = np.interp(zgrid, prior[0], prior[1])
+            chi2 += interp_prior
+        else:
+            interp_prior = None
+            
+        print(' Zoom iteration: z_best={0:.4f}\n'.format(zgrid[np.argmin(chi2)]))
+        
+        ### Best redshift
+        if not stars:
+            templates = utils.load_templates(line_complexes=False, fwhm=fwhm, fsps_templates=fsps_templates)
+        
+        zbest = zgrid[np.argmin(chi2)]
+        ix = np.argmin(chi2)
+        chibest = chi2.min()
+        
+        ## Fit parabola
+        if (ix > 0) & (ix < len(chi2)-1):
+            c = polyfit(zgrid[ix-1:ix+2], chi2[ix-1:ix+2], 2)
+            zbest = -c[1]/(2*c[0])
+            chibest = polyval(c, zbest)
+        
+        out = self.fit_at_z(z=zbest, templates=templates,
+                            fitter=fitter, poly_order=poly_order, 
+                            fit_background=fit_background)
+        
+        A, coeffs_full, chi2_best, model_full = out
+        
+        # Parse results
+        out2 = self.parse_fit_outputs(zbest, templates, coeffs_full, A)
+        line_flux, covar, cont1d, line1d, model1d, model_continuum = out2
+        
+        # Output dictionary with fit parameters
+        fit_data = OrderedDict()
+        fit_data['poly_order'] = poly_order
+        fit_data['fwhm'] = fwhm
+        fit_data['zbest'] = zbest
+        fit_data['chibest'] = chibest
+        fit_data['chi_poly'] = chi2_poly
+        fit_data['zgrid'] = zgrid
+        fit_data['prior'] = interp_prior
+        fit_data['A'] = A
+        fit_data['coeffs'] = coeffs
+        fit_data['chi2'] = chi2
+        fit_data['DoF'] = self.DoF
+        fit_data['model_full'] = model_full
+        fit_data['coeffs_full'] = coeffs_full
+        fit_data['covar'] = covar
+        fit_data['line_flux'] = line_flux
+        #fit_data['templates_full'] = templates
+        fit_data['model_cont'] = model_continuum
+        fit_data['model1d'] = model1d
+        fit_data['cont1d'] = cont1d
+        fit_data['line1d'] = line1d
 class MultiBeam(GroupFitter):
     def __init__(self, beams, group_name='group', fcontam=0., psf=False):
         """Tools for dealing with multiple `~.model.BeamCutout` instances 
@@ -1009,6 +1349,8 @@ class MultiBeam(GroupFitter):
         self.DoF = self.fit_mask.sum()
         self.ivarf = np.hstack([b.ivarf for b in self.beams])
         self.ivarf[~np.isfinite(self.ivarf)] = 0
+        self.sivarf = np.sqrt(self.ivarf)
+        
         self.fit_mask &= (self.ivarf >= 0) 
         
         self.scif = np.hstack([b.scif for b in self.beams])
@@ -1025,6 +1367,7 @@ class MultiBeam(GroupFitter):
             i0 += self.Nflat[i]
         
         self.init_poly_coeffs(poly_order=1)
+        self.slices = self._get_slices()
         
         self.ra, self.dec = self.beams[0].get_sky_coords()
     
@@ -1214,7 +1557,7 @@ class MultiBeam(GroupFitter):
             i0 = 0            
             for ib in range(self.N):
                 beam = self.beams[ib]
-                lam_beam = beam.wave
+                lam_beam = beam.beam.lam_beam
                 if ((temp.wave.min()*(1+z) > lam_beam.max()) | 
                     (temp.wave.max()*(1+z) < lam_beam.min())):
                     tmodel = 0.
@@ -2115,7 +2458,7 @@ class MultiBeam(GroupFitter):
                                            is_cgs=True)
                         
                         beam.oivar = beam.ivar*1
-                        lam = beam.wave
+                        lam = beam.beam.lam_beam
                         
                         ### another idea, compute a model for the line itself
                         ### and mask relatively "contaminated" pixels from 
@@ -2435,7 +2778,7 @@ class MultiBeam(GroupFitter):
         print(shifts, chi2/self.DoF)
         return chi2/self.DoF    
     
-    def drizzle_grisms_and_PAs(self, size=10, fcontam=0, flambda=True, scale=1, pixfrac=0.5, kernel='square', make_figure=True, usewcs=False, zfit=None):
+    def drizzle_grisms_and_PAs(self, size=10, fcontam=0, flambda=True, scale=1, pixfrac=0.5, kernel='square', make_figure=True, usewcs=False, zfit=None, diff=True):
         """Make figure showing spectra at different orients/grisms
         
         TBD
@@ -2685,7 +3028,7 @@ class MultiBeam(GroupFitter):
             output_hdu.extend(hdu)
             
         if make_figure:
-            fig = show_drizzle_HDU(output_hdu)
+            fig = show_drizzle_HDU(output_hdu, diff=diff)
             return output_hdu, fig
         else:
             return output_hdu #all_hdus
