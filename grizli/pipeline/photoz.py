5536245426d08358ee5ee4a8c73a835927ed6a70
Gabriel Brammer <gbrammer@gmail.com>
2019-01-29 15:28:19 +0100

    Testing selecting high-z objcts

--- grizli/pipeline/photoz.py
+++ grizli/pipeline/photoz.py
@@ -1,4 +1,4 @@
-def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper_ix=1, apply_prior=True, beta_prior=True, get_external_photometry=True, external_limits=3, external_sys_err=0.3, external_timeout=300, sys_err=0.05, z_step=0.01, z_min=0.01, z_max=12, total_flux='flux'):
+def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper_ix=1, apply_prior=True, beta_prior=True, get_external_photometry=True, external_limits=3, external_sys_err=0.3, external_timeout=300, sys_err=0.05, z_step=0.01, z_min=0.01, z_max=12, total_flux='flux_auto_fix', compute_residuals=True, dummy_prior=True):
     
     import os
     import eazy
@@ -13,7 +13,7 @@ def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper
         cat = utils.read_catalog('{0}_phot_apcorr.fits'.format(root))
         return self, cat, zout
         
-    trans = {'f098m':201, 'f105w':202, 'f110w':241, 'f125w':203, 'f140w':204, 'f160w':205, 'f435w':233, 'f438w':211, 'f606w':236, 'f625w':237, 'f814w':239, 'f702w':15, 'f555w':235, 'f350lp':339, 'f475w':212}
+    trans = {'f098m':201, 'f105w':202, 'f110w':241, 'f125w':203, 'f140w':204, 'f160w':205, 'f435w':233, 'f438w':211, 'f606w':236, 'f625w':237, 'f814w':239, 'f702w':15, 'f555w':235, 'f350lp':339, 'f475w':212, 'f775w':238}
     
     cat = utils.read_catalog('{0}_phot.fits'.format(root))
     filters = []
@@ -38,6 +38,14 @@ def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper
             pass
         
     # Total flux
+    
+    # Fix: Take flux_auto when flag==0, flux otherwise
+    if (total_flux == 'flux_auto_fix') & (total_flux not in cat.colnames):
+        flux = cat['flux_auto']*1.
+        flagged = (cat['flag'] > 0)
+        flux[flagged] = cat['flux'][flagged]
+        cat['flux_auto_fix'] = flux*1.
+        
     cat.meta['TOTALCOL'] = total_flux, 'Column for total flux'
     
     apcorr = {}
@@ -82,7 +90,10 @@ def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper
         # Should be >~ 0.9 for valid sources, stars & galaxies in "ir" image
         cat['class_valid'] = pred[:,-3:].sum(axis=1) 
         cat['class_valid'].format = '.2f'
-        
+    
+    cat['dummy_err'] =  10**(-0.4*(8-23.9))
+    cat['dummy_flux'] = cat[total_flux] # detection band
+    
     cat.write('{0}_phot_apcorr.fits'.format(root), overwrite=True)
     
     # Translate
@@ -91,6 +102,11 @@ def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper
         if f in trans:
             fp.write('{0}_corr_{1} F{2}\n'.format(f, aper_ix, trans[f]))
             fp.write('{0}_ecorr_{1} E{2}\n'.format(f, aper_ix, trans[f]))
+    
+    # For zeropoint
+    if dummy_prior:
+        fp.write('dummy_flux F205x\n')
+        fp.write('dummy_err  E205x\n')
             
     fp.close()
     
@@ -109,11 +125,16 @@ def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper
     # Pick prior filter, starting from reddest
     for f in ['f435w', 'f606w', 'f814w', 'f105w', 'f110w', 'f125w', 'f140w', 'f160w'][::-1]:
         if f in filters:
-            params['PRIOR_FILTER'] = trans[f]
+            if dummy_prior:
+                params['PRIOR_FILTER'] = 'dummy_flux'
+            else:
+                params['PRIOR_FILTER'] = trans[f]
+            
             mag = 23.9-2.5*np.log10(cat['{0}_corr_{1}'.format(f, aper_ix)])
             break
     #
-    params['TEMPLATES_FILE'] = 'templates/fsps_full/tweak_fsps_QSF_12_v3.param'
+   # params['TEMPLATES_FILE'] = 'templates/fsps_full/tweak_fsps_QSF_12_v3.param'
+    params['TEMPLATES_FILE'] = 'templates/fsps_full/tweak_fsps_QSF_11_v3_noRed.param'
 
     zpfile = None
     load_products = False
@@ -129,11 +150,12 @@ def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper
     
     #sample = (mag < 27) #& (self.cat['star_flag'] != 1)
     #sample |= (self.cat['z_spec'] > 0)
-    sample = np.isfinite(mag)
+    sample = np.isfinite(self.cat['id']) #mag)
     
-    for iter in range(2):
+    for iter in range(1+(get_external_photometry & compute_residuals)*1):
         self.fit_parallel(idx[sample], n_proc=10)
-        self.error_residuals()
+        if compute_residuals:
+            self.error_residuals()
     
     self.standard_output(prior=apply_prior, beta_prior=beta_prior)
     
@@ -340,4 +362,136 @@ def get_external_catalog(phot, filter_file='/usr/local/share/eazy-photoz/filters
             
     return extern_phot
     
+########### Selecting objects
+def select_objects():
+    self, cat, zout = photoz.eazy_photoz(root, object_only=False, apply_prior=False, beta_prior=True, aper_ix=1, force=True, get_external_photometry=False, compute_residuals=False, total_flux='flux_auto_fix')
+
+    flux = self.cat['flux_auto_fix']*1.
+    hmag = 23.9-2.5*np.log10(flux)
+
+    # Reddest HST band
+    lc_clip = self.lc*1
+    lc_clip[self.lc > 1.55e4] = 0 # in case ground-based / WISE red band
+    ixb = np.argmax(lc_clip)
+    sn_red = self.cat[self.flux_columns[ixb]]/self.cat[self.err_columns[ixb]]
+
+    grad = np.gradient(self.zgrid)
+    cumpz = np.cumsum(self.pz*grad, axis=1); cumpz = (cumpz.T/cumpz[:,-1]).T
+
+    chi2 = (self.chi_best / self.nusefilt)
+
+    iz6 = np.where(self.zgrid > 6.5)[0][0]
+    iz8 = np.where(self.zgrid > 8)[0][0]
+    iz10 = np.where(self.zgrid > 9)[0][0]
+
+    highz_sel = (cumpz[:,iz6] > 0) & ((cumpz[:,iz6] < 0.3) | (cumpz[:,iz8] < 0.5) | (cumpz[:,iz10] < 0.6)) 
+    highz_sel &= (hmag < 27.5) & (self.cat['class_valid'] > 0.8)
+    highz_sel &= (self.cat['flux_radius'] > 2.5) 
+    highz_sel &= chi2 < 3
+    highz_sel &= (sn_red > 5)
+
+    # Big objects likely diffraction spikes
+    # big = (self.cat['flux_radius'] > 10)
+    # highz_sel &= ~big
+    #flux_ratio = self.cat['flux_aper_0']/self.cat['flux_aper_2']
+
+    sel = highz_sel
+    so = np.argsort(hmag[sel]); ids = self.cat['id'][sel][so]; i=-1
+
+    # Red 
+    uv = -2.5*np.log10(zout['restU']/zout['restV'])
+    red_sel = ((zout['z160'] > 1.) & (uv > 1.5)) | ((zout['z160'] > 1.5) & (uv > 1.1))
+    red_sel &= (self.zbest < 4) & (hmag < 24) 
+    red_sel &= (zout['mass'] > 10**10.5) & (self.cat['class_valid'] > 0.8)
+    red_sel &= (self.cat['flux_radius'] > 2.5)
+    red_sel &= (zout['restV']/zout['restV_err'] > 3)
+    red_sel &= (chi2 < 3)
+    red_sel &= (sn_red > 20)
+
+    sel = red_sel
+
+    so = np.argsort(hmag[sel]); ids = self.cat['id'][sel][so]; i=-1
+
+    for j in self.idx[sel][so]:
+        id_j, ra, dec = self.cat['id', 'ra', 'dec'][j]
+
+        # Photo-z
+        fig = self.show_fit(id_j, ds9=None, show_fnu=highz_sel[j])    
+        lab = '{0} {1}\n'.format(root, id_j)
+        lab += 'H={0:.1f} z={1:.1f}\n'.format(hmag[j], self.zbest[j])
+        lab += 'U-V={0:.1f}, logM={1:4.1f}'.format(uv[j], np.log10(zout['mass'][j]))
+
+        ax = fig.axes[0]
+        ax.text(0.95, 0.95, lab, ha='right', va='top', transform=ax.transAxes, fontsize=9, bbox=dict(facecolor='w', edgecolor='None', alpha=0.5))
+        yl = ax.get_ylim()
+        ax.set_ylim(yl[0], yl[1]*1.1)
+
+        fig.savefig('{0}_{1:05d}.eazy.png'.format(root, id_j), dpi=70)
+        plt.close()
+
+        # Cutout
+        from grizli_aws.aws_drizzle import drizzle_images
+        
+        #rgb_params = {'output_format': 'png', 'output_dpi': 75, 'add_labels': False, 'show_ir': False, 'suffix':'.rgb'}
+        rgb_params = None
+
+        #aws_bucket = 's3://grizli/SelectedObjects/'
+        aws_bucket = None
+
+        label = '{0}_{1:05d}'.format(root, id_j)
+        if not os.path.exists('{0}.rgb.png'.format(label)):
+            drizzle_images(label=label, ra=ra, dec=dec, pixscale=0.06, size=8, pixfrac=0.8, theta=0, half_optical_pixscale=False, filts=['f160w', 'f814w', 'f140w', 'f125w', 'f105w', 'f110w', 'f098m', 'f850lp', 'f775w', 'f606w', 'f475w'], remove=False, rgb_params=rgb_params, master='grizli-jan2019', aws_bucket=aws_bucket)
+
+        show_all_thumbnails(label=label, filters=['f775w','f814w','f098m','f105w','f110w','f125w','f140w','f160w'], scale_ab=np.clip(hmag[j],19,22), close=True)
+
+############
+def show_all_thumbnails(label='j022708p4901_00273', filters=['f775w','f814w','f098m','f105w','f110w','f125w','f140w','f160w'], scale_ab=21, close=True):
+    """
+    Show individual filter and RGB thumbnails
+    """
+    from astropy.visualization import make_lupton_rgb
+    from PIL import Image
+    
+    ims = {}
+    for filter in filters:
+        drz_files = glob.glob('{0}-{1}*_dr*sci.fits'.format(label, filter))
+        if len(drz_files) > 0:
+            im = pyfits.open(drz_files[0])
+            ims[filter] = im
+            
+    slx, sly, filts, fig = auto_script.field_rgb(root=label, xsize=4, output_dpi=None, HOME_PATH=None, show_ir=False, pl=1, pf=1, scl=1, rgb_scl=[1, 1, 1], ds9=None, force_ir=False, filters=ims.keys(), add_labels=False, output_format='png', rgb_min=-0.01, xyslice=None, pure_sort=False, verbose=True, force_rgb=None, suffix='.rgb', scale_ab=scale_ab)
+    if close:
+        plt.close()
+    
+    rgb = np.array(Image.open('{0}.rgb.png'.format(label)))
+    
+    NX = (len(filters)+1)
+    fig = plt.figure(figsize=[1.5*NX,1.5])
+    ax = fig.add_subplot(1,NX,NX)
+    ax.imshow(rgb, origin='upper', interpolation='nearest')
+    ax.text(0.5, 0.95, label, ha='center', va='top', transform=ax.transAxes, fontsize=7, bbox=dict(facecolor='w', edgecolor='None', alpha=0.5))
+    
+    for i, filter in enumerate(filters):
+        if filter in ims:
+            zp_i = utils.calc_header_zeropoint(ims[filter], ext=0)
+            scl = 10**(-0.4*(zp_i-5-scale_ab))
+            img = ims[filter][0].data*scl
+            image = make_lupton_rgb(img, img, img, stretch=0.1, minimum=-0.01)
+            
+            ax = fig.add_subplot(1,NX,i+1)
+            ax.imshow(255-image, origin='lower', interpolation='nearest')
+            
+            ax.text(0.5, 0.95, filter, ha='center', va='top', transform=ax.transAxes, fontsize=7, bbox=dict(facecolor='w', edgecolor='None', alpha=0.5))
+    
+    for ax in fig.axes:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    fig.tight_layout(pad=0.1)
+    
+    fig.savefig('{0}.thumb.png'.format(label))
+    if close:
+        plt.close()
         
