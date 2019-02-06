f6f9e63407215fc83588b4cae4b7304b04f94a98
Gabriel Brammer <gbrammer@gmail.com>
2019-02-06 20:24:52 +0100

    Accept file list for auto_script.summary_catalog

--- grizli/pipeline/auto_script.py
+++ grizli/pipeline/auto_script.py
@@ -1906,13 +1906,13 @@ def set_column_formats(fit):
     """
     Set print formats for the master catalog columns
     """
-    fit['ellipticity'].format = '.2f'
     
     for c in ['ra', 'dec']:
         fit[c].format = '.5f'
     
-    for col in ['MAG_AUTO', 'FLUX_RADIUS', 'A_IMAGE']:
-        fit[col.lower()].format = '.2f'
+    for col in ['MAG_AUTO', 'FLUX_RADIUS', 'A_IMAGE', 'ellipticity']:
+        if col.lower() in fit.colnames:
+            fit[col.lower()].format = '.2f'
             
     for c in ['log_risk', 'log_pdf_max', 'zq','chinu', 'bic_diff']:
         fit[c].format = '.2f'
@@ -1945,7 +1945,7 @@ def set_column_formats(fit):
         
     return fit
     
-def summary_catalog(field_root='', dzbin=0.01, use_localhost=True, filter_bandpasses=None):
+def summary_catalog(field_root='', dzbin=0.01, use_localhost=True, filter_bandpasses=None, files=None):
     """
     Make redshift histogram and summary catalog / HTML table
     """
@@ -1955,7 +1955,7 @@ def summary_catalog(field_root='', dzbin=0.01, use_localhost=True, filter_bandpa
     import matplotlib.pyplot as plt
     
     try:
-        from .. import fitting
+        from .. import fitting, prep, utils
         from . import auto_script
     except:
         from grizli import prep, utils, fitting
@@ -1967,24 +1967,28 @@ def summary_catalog(field_root='', dzbin=0.01, use_localhost=True, filter_bandpa
         
     ### SUmmary catalog
     fit = fitting.make_summary_catalog(target=field_root, sextractor=None,
-                                       filter_bandpasses=filter_bandpasses)
+                                       filter_bandpasses=filter_bandpasses,
+                                       files=files)
                                        
     fit.meta['root'] = field_root
     
     ## Add photometric catalog
-    sex = utils.GTable.gread('{0}-ir.cat.fits'.format(field_root))
-    # try:
-    # except:
-    #     sex = utils.GTable.gread('../Prep/{0}-ir.cat.fits'.format(field_root), sextractor=True)
+    try:
+        sex = utils.GTable.gread('{0}-ir.cat.fits'.format(field_root))
+        # try:
+        # except:
+        #     sex = utils.GTable.gread('../Prep/{0}-ir.cat.fits'.format(field_root), sextractor=True)
         
-    idx = np.arange(len(sex))
-    sex_idx = np.array([idx[sex['NUMBER'] == id][0] for id in fit['id']])
+        idx = np.arange(len(sex))
+        sex_idx = np.array([idx[sex['NUMBER'] == id][0] for id in fit['id']])
     
-    fit['ellipticity'] = (sex['B_IMAGE']/sex['A_IMAGE'])[sex_idx]
+        fit['ellipticity'] = (sex['B_IMAGE']/sex['A_IMAGE'])[sex_idx]
         
-    for col in ['MAG_AUTO', 'FLUX_RADIUS', 'A_IMAGE']:
-        fit[col.lower()] = sex[col][sex_idx]
-
+        for col in ['MAG_AUTO', 'FLUX_RADIUS', 'A_IMAGE']:
+            fit[col.lower()] = sex[col][sex_idx]
+    except:
+        pass
+        
     fit = set_column_formats(fit)
     
     # Overwrite with additional sextractor keywords
@@ -2017,8 +2021,11 @@ def summary_catalog(field_root='', dzbin=0.01, use_localhost=True, filter_bandpa
     fig.tight_layout(pad=0.2)
     fig.savefig('{0}_zhist.png'.format(field_root))
 
-    cols = ['idx','ra', 'dec', 'mag_auto', 't_g800l', 't_g102', 't_g141', 'z_map', 'chinu', 'bic_diff', 'zwidth1', 'stellar_mass', 'ssfr', 'png_stack', 'png_full', 'png_line']
-    
+    cols = ['root', 'idx','ra', 'dec', 'mag_auto', 't_g800l', 't_g102', 't_g141', 'z_map', 'chinu', 'bic_diff', 'zwidth1', 'stellar_mass', 'ssfr', 'png_stack', 'png_full', 'png_line']
+    for i in range(len(cols))[::-1]:
+        if cols[i] not in fit.colnames:
+            cols.pop(i)
+            
     fit[cols].write_sortable_html(field_root+'-fit.html', replace_braces=True, localhost=use_localhost, max_lines=50000, table_id=None, table_class='display compact', css=None)
         
     fit[cols][clip].write_sortable_html(field_root+'-fit.zq.html', replace_braces=True, localhost=use_localhost, max_lines=50000, table_id=None, table_class='display compact', css=None)
