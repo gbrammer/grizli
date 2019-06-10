b6f70e804f98366bd2c2b2dd212beff561b5d91a
Gabriel Brammer <gbrammer@gmail.com>
2019-06-10 23:45:41 +0200

    Pipeline fixes, nearly at production run

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ version = p.communicate()[0].decode("utf-8").strip()
 # version = "0.10.0" # Relatively small fixes, fix bug in 1D wave
 # version = "0.11.0" # Refactored parameter files
 # version = "0.12.0" # Increment to fix tag tar files
+version = "0.13.0" # Various pipeline modifications, add aws scripts
 
 version_str = """# git describe --tags
 __version__ = "{0}"\n""".format(version)
