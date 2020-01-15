319983cd826c8694f4c3e33a72cd14080f12d3b7
Gabriel Brammer <gbrammer@gmail.com>
2020-01-15 14:40:59 +0100

    fix filter

--- grizli/pipeline/photoz.py
+++ grizli/pipeline/photoz.py
@@ -138,7 +138,7 @@ def apply_catalog_corrections(root, total_flux='flux_auto', auto_corr=True, get_
     
     return cat
     
-def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper_ix=1, apply_prior=False, beta_prior=True, get_external_photometry=False, external_limits=3, external_sys_err=0.3, external_timeout=300, sys_err=0.05, z_step=0.01, z_min=0.01, z_max=12, total_flux='flux_auto', auto_corr=True, compute_residuals=False, dummy_prior=False, extra_rf_filters=[], quiet=True):
+def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper_ix=1, apply_prior=False, beta_prior=True, get_external_photometry=False, external_limits=3, external_sys_err=0.3, external_timeout=300, sys_err=0.05, z_step=0.01, z_min=0.01, z_max=12, total_flux='flux_auto', auto_corr=True, compute_residuals=False, dummy_prior=False, extra_rf_filters=[], quiet=True, aperture_indices='all'):
     
     import os
     import eazy
@@ -153,12 +153,12 @@ def eazy_photoz(root, force=False, object_only=True, apply_background=True, aper
         cat = utils.read_catalog('{0}_phot_apcorr.fits'.format(root))
         return self, cat, zout
         
-    trans = {'f098m':201, 'f105w':202, 'f110w':241, 'f125w':203, 'f140w':204, 'f160w':205, 'f435w':233, 'f475w':234, 'f555w':235, 'f606w':236, 'f625w':237, 'f775w':238, 'f814w':239, 'f850lp':240, 'f702w':15, 'f600lpu':243, 'f225wu':207, 'f275wu':208, 'f336w':209, 'f350lpu':339, 'f438wu':211, 'f475wu':212, 'f475xu':242, 'f555wu':213, 'f606wu':214, 'f625wu':215, 'f775wu':216, 'f814wu':217, 'f390wu':210, 'ch1':18, 'ch2':19}
+    trans = {'f098m':201, 'f105w':202, 'f110w':241, 'f125w':203, 'f140w':204, 'f160w':205, 'f435w':233, 'f475w':234, 'f555w':235, 'f606w':236, 'f625w':237, 'f775w':238, 'f814w':239, 'f850lp':240, 'f702w':15, 'f600lpu':243, 'f225wu':207, 'f275wu':208, 'f336wu':209, 'f350lpu':339, 'f438wu':211, 'f475wu':212, 'f475xu':242, 'f555wu':213, 'f606wu':214, 'f625wu':215, 'f775wu':216, 'f814wu':217, 'f390wu':210, 'ch1':18, 'ch2':19}
     
     #trans.pop('f814w')
     
     print('Apply catalog corrections')
-    apply_catalog_corrections(root, suffix='_apcorr')
+    apply_catalog_corrections(root, suffix='_apcorr', aperture_indices=aperture_indices)
     
     cat = utils.read_catalog('{0}_phot_apcorr.fits'.format(root))
     filters = []
