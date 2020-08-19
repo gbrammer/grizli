f5dd64a206f1750440942f1e81cbd4b2d216a1c4
Gabriel Brammer <gbrammer@gmail.com>
2020-08-19 12:56:33 +0200

    ds9 query utilities

--- grizli/ds9.py
+++ grizli/ds9.py
@@ -1,6 +1,7 @@
 """
 Extensions of the `~pyds9.DS9` object
 """
+import os
 
 # Extend pyds9.DS9 class
 try:
@@ -11,53 +12,103 @@ except:
     print('NB: Couldn\'t `import pyds9`.  The `grizli.ds9.DS9` object will be broken')
     ds9_obj = object
 
-
+def show_open_targets():
+    """
+    Summarize open ds9 targets
+    """
+    import pyds9
+    import numpy as np
+    
+    targets = pyds9.ds9_targets()
+    print('i         target frame    ra       dec       x     y ')
+    for i, target in enumerate(targets):
+        targ = target.split()[-1]
+        ds9 = DS9(target=targ)
+        ra, dec = np.cast[float](ds9.get('pan fk5').split())
+        xi, yi = np.cast[float](ds9.get('pan image').split())
+        
+        frame = ds9.get('frame')
+        print(f'{i} {targ:14} {frame:>5} {ra:8.4f} {dec:8.4f} ({xi:5.0f} {yi:5.0f})')
+        
+        
+def get_ds9(i=-1):
+    """
+    Get ds9 xpa target
+    
+    Parameters
+    ----------
+    i : int
+        Index of target in `~pyds9.ds9_targets`  list.
+    
+    Returns
+    -------
+    obj : `~grizli.ds9.DS9`
+        DS9 object.
+    """
+    import pyds9
+    targets = pyds9.ds9_targets()
+    #print(targets)
+    obj = DS9(target=targets[i].split()[-1])
+    return obj
+    
 class DS9(ds9_obj):
     """
     Extend `~pyds9.DS9` object with convenience classes
     """
 
-    def view(self, img, header=None):
+    def view(self, img, header=None, simplify_wcs=True):
         """
         From pysao
         """
+        import astropy.wcs as pywcs
+        from ..utils import to_header
+        
         if hasattr(img, 'header'):
             # FITS HDU
             self.set_np2arr(img.data)
             if not header:
                 header = img.header
-            self.set("wcs replace", header.tostring())
         else:
             self.set_np2arr(img)
-            if header:
+        
+        if header:
+            if simplify_wcs:
+                wcs = pywcs.WCS(header)
+                simple_header = to_header(wcs)
+                self.set("wcs replace", simple_header.tostring())
+            else:
                 self.set("wcs replace", header.tostring())
-
+                
     def frame(self, id):
-        self.set('frame %d' % (id))
+        self.set(f'frame {frame}')
 
     def scale(self, z1, z2):
-        self.set('scale limits %f %f' % (z1, z2))
+        self.set(f'scale limits {z1} {z2}')
 
     def set_defaults(self, match='image', verbose=False):
         """
         Match frame, set log scale
         """
-        commands = """
+        commands = f"""
         xpaset -p ds9 scale log
         xpaset -p ds9 scale limits -0.1 10
         xpaset -p ds9 cmap value 3.02222 0.647552
-        xpaset -p ds9 match frames %s
-        xpaset -p ds9 frame lock %s
-        xpaset -p ds9 match colorbars
+        xpaset -p ds9 match frame {match}
+        xpaset -p ds9 frame lock {match}
+        xpaset -p ds9 match colorbar
         xpaset -p ds9 lock colorbar
-        xpaset -p ds9 match scales""" % (match, match)
+        xpaset -p ds9 match scale"""
 
         for c in commands.split('\n'):
             if 'xpaset' in c:
-                self.set(' '.join(c.split()[3:]))
+                try:
+                    self.set(' '.join(c.split()[3:]))
+                except:
+                    print('xpa command failed: ', c)
                 if verbose:
                     print(c)
-
+    
+    
     def match(self, match='image'):
         commands = """
         xpaset -p ds9 match frames %s
@@ -67,3 +118,72 @@ class DS9(ds9_obj):
         for c in commands.split('\n'):
             if 'xpaset' in c:
                 self.set(' '.join(c.split()[3:]))
+    
+    
+    def cds_query(self, radius=1.):
+        """
+        Open browswer with CDS catalog query around central position
+        """
+        rd = self.get('pan fk5').strip()
+        rdst = rd.replace('+', '%2B').replace('-', '%2D').replace(' ', '+')
+        url = (f'"http://vizier.u-strasbg.fr/viz-bin/VizieR?'
+               f'-c={rdst}&-c.rs={radius:.1f}"')
+               
+        os.system(f'open {url}')
+    
+    
+    def eso_query(self, radius=1., dp_types=['CUBE','IMAGE'], extra=''):
+        """
+        Open browser with ESO archive query around central position.
+        
+        ``radius`` in arcmin.
+        """
+        ra, dec = self.get('pan fk5').strip().split()
+        
+        dp_type = ','.join(dp_types)
+        
+        url = (f'"https://archive.eso.org/scienceportal/home?'
+                f'pos={ra},{dec}&r={radius/60.}&dp_type={dp_type}{extra}"')
+                        
+        os.system(f'open {url}')
+    
+    
+    def mast_query(self, instruments=['WFC3','ACS','WFPC2'], max=1000):
+        """
+        Open browser with MAST archive query around central position
+        """
+        ra, dec = self.get('pan fk5').strip().split()
+        if len(instruments) > 0:
+            instr='&sci_instrume='+','.join(instruments)
+        else:
+            instr = ''
+            
+        url = (f'"https://archive.stsci.edu/hst/search.php?RA={ra}&DEC={dec}'
+               f'&sci_aec=S{instr}&max_records={max}&outputformat=HTML_Table'
+                '&action=Search"')
+                
+        os.system(f'open {url}')
+    
+    
+    def alma_query(self, mirror="almascience.eso.org", extra=''):
+        """
+        Open browser with ALMA archive query around central position
+        """
+        ra, dec = self.get('pan fk5').strip().split()
+    
+        url = (f"https://{mirror}/asax/?result_view=observation"
+               f"&raDec={ra}%20{dec}{extra}")
+        os.system(f'open "{url}"')
+    
+    
+    def show_legacysurvey(self, layer='dr8', zoom=14):
+        """
+        Open browser with legacysurvey.org panner around central position
+        """
+        ra, dec = self.get('pan fk5').strip().split()
+        url = (f'"http://legacysurvey.org/viewer?ra={ra}&dec={dec}'
+               f'&layer={layer}&zoom={zoom}"')
+                
+        os.system(f'open {url}')
+        
+        
