adf4ce4456caf8b980cd939b947f1990f553ace0
Gabriel Brammer <gbrammer@gmail.com>
2022-03-04 02:14:01 +0100

    use boto3

--- grizli/aws/db.py
+++ grizli/aws/db.py
@@ -110,6 +110,33 @@ def get_db_engine(config=None, echo=False, iam_file='/home/ec2-user/db.iam.yaml'
     return engine
 
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+    import logging
+    import boto3
+    from botocore.exceptions import ClientError
+    import os
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
 def get_redshift_fit_status(root, id, table='redshift_fit', engine=None):
     """
     Get status value from the database for root_id object
