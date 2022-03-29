c3556512f794c84cd83b67bd8f7f853a8cd1324e
Gabriel Brammer <gbrammer@gmail.com>
2022-03-29 11:32:24 +0200

    use noao source catalog if nothing else found

--- grizli/aws/visit_processor.py
+++ grizli/aws/visit_processor.py
@@ -662,7 +662,7 @@ def launch_ec2_instances(nmax=50, templ='lt-0e8c2b8611c9029eb,Version=11'):
                       FROM assoc_table
                       WHERE status = 0""")
     
-    count = np.minimum(nmax, len(assoc))
+    count = np.minimum(nmax, len(assoc)/2)
 
     if count == 0:
         print('No associations to run, abort.')
@@ -948,7 +948,7 @@ blue_align_params['tweak_n_min'] = 5
 ALL_FILTERS = ['F410M', 'F467M', 'F547M', 'F550M', 'F621M', 'F689M', 'F763M', 'F845M', 'F200LP', 'F350LP', 'F435W', 'F438W', 'F439W', 'F450W', 'F475W', 'F475X', 'F555W', 'F569W', 'F600LP', 'F606W', 'F622W', 'F625W', 'F675W', 'F702W', 'F775W', 'F791W', 'F814W', 'F850LP', 'G800L', 'F098M', 'F127M', 'F139M', 'F153M', 'F105W', 'F110W', 'F125W', 'F140W', 'F160W', 'G102', 'G141']
 
 
-def process_visit(assoc, clean=True, sync=True, max_dt=4, visit_split_shift=1.2, blue_align_params=blue_align_params, ref_catalogs=['LS_DR9', 'PS1', 'DES', 'GAIA'], filters=None, prep_args={'align_mag_limits':[14,23,0.1]}, get_wcs_guess_from_table=True, master_radec=None, **kwargs):
+def process_visit(assoc, clean=True, sync=True, max_dt=4, visit_split_shift=1.2, blue_align_params=blue_align_params, ref_catalogs=['LS_DR9', 'PS1', 'DES', 'NSC', 'GAIA'], filters=None, prep_args={'align_mag_limits':[14,23,0.1]}, get_wcs_guess_from_table=True, master_radec=None, **kwargs):
     """
     `assoc_table.status`
     
