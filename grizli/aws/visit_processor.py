137b492f294e8ea1be9434f9c77661b962613495
Gabriel Brammer <gbrammer@gmail.com>
2022-03-30 15:28:16 +0200

    add combine_same_pa parameter

--- grizli/aws/visit_processor.py
+++ grizli/aws/visit_processor.py
@@ -935,7 +935,7 @@ def get_master_radec(s_region, bucket='grizli-v2', prefix='HST/Pipeline/Astromet
 
 
 blue_align_params={}
-blue_align_params['align_mag_limits'] = [20,25,0.12]
+blue_align_params['align_mag_limits'] = [18,25,0.15]
 blue_align_params['align_simple'] = False
 blue_align_params['max_err_percentile'] = 80
 blue_align_params['catalog_mask_pad'] = 0.05
@@ -948,7 +948,7 @@ blue_align_params['tweak_n_min'] = 5
 ALL_FILTERS = ['F410M', 'F467M', 'F547M', 'F550M', 'F621M', 'F689M', 'F763M', 'F845M', 'F200LP', 'F350LP', 'F435W', 'F438W', 'F439W', 'F450W', 'F475W', 'F475X', 'F555W', 'F569W', 'F600LP', 'F606W', 'F622W', 'F625W', 'F675W', 'F702W', 'F775W', 'F791W', 'F814W', 'F850LP', 'G800L', 'F098M', 'F127M', 'F139M', 'F153M', 'F105W', 'F110W', 'F125W', 'F140W', 'F160W', 'G102', 'G141']
 
 
-def process_visit(assoc, clean=True, sync=True, max_dt=4, visit_split_shift=1.2, blue_align_params=blue_align_params, ref_catalogs=['LS_DR9', 'PS1', 'DES', 'NSC', 'GAIA'], filters=None, prep_args={'align_mag_limits':[14,23,0.1]}, get_wcs_guess_from_table=True, master_radec=None, **kwargs):
+def process_visit(assoc, clean=True, sync=True, max_dt=4, combine_same_pa=True, visit_split_shift=1.2, blue_align_params=blue_align_params, ref_catalogs=['LS_DR9', 'PS1', 'DES', 'NSC', 'GAIA'], filters=None, prep_args={'align_mag_limits':[14,23,0.1]}, get_wcs_guess_from_table=True, master_radec=None, **kwargs):
     """
     `assoc_table.status`
     
@@ -1014,6 +1014,7 @@ def process_visit(assoc, clean=True, sync=True, max_dt=4, visit_split_shift=1.2,
     if True:
         kws['parse_visits_args']['max_dt'] = max_dt
         kws['parse_visits_args']['visit_split_shift'] = visit_split_shift
+        kws['parse_visits_args']['combine_same_pa'] = combine_same_pa
         
     if ('_f4' in assoc) | ('_f3' in assoc) | ('_f2' in assoc) & (blue_align_params is not None):        
         for k in blue_align_params:
