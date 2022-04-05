baf53da0ca6435830be3f5f34ef1e7dbf0e20011
Victoria Strait <victoria.b.strait@gmail.com>
2022-04-05 14:15:48 +0200

    removed newline in grismconf

--- grizli/grismconf.py
+++ grizli/grismconf.py
@@ -506,7 +506,6 @@ def get_config_filename(instrume='WFC3', filter='F140W',
         String path of the configuration file.
 
     """
-
     if instrume == 'ACS':
         conf_file = os.path.join(GRIZLI_PATH,
                     'CONF/ACS.WFC.CHIP{0:d}.Stars.conf'.format(chip))
