b8d9170f767b826d893b64d7c4869f5995ab5fdf
Gabriel Brammer <gbrammer@gmail.com>
2022-07-14 16:07:27 +0200

    fix bug

--- grizli/pipeline/auto_script.py
+++ grizli/pipeline/auto_script.py
@@ -975,7 +975,7 @@ def fetch_files(field_root='j142724+334246', HOME_PATH='$PWD', paths={}, inst_pr
             ## Download from MAST API when data available xxx            
             _resp = mastquery.utils.download_from_mast(tab[jw])
             # update targname
-            for im in _rep:
+            for im in _resp:
                 if os.path.exists(_resp) & (jwst_utils is not None):
                     jwst_utils.initialize_jwst_image(_resp)
                     
