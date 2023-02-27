a1946ed51bd911196d025711628c9f2e6842afd8
Gabriel Brammer <gbrammer@gmail.com>
2023-02-27 14:31:55 +0100

    add F150W2 F160M for wisp correction

--- grizli/prep.py
+++ grizli/prep.py
@@ -2860,8 +2860,9 @@ def get_nircam_wisp_filename(header):
     if _inst not in ['NIRCAM']:
         msg = f'Instrument {_inst} not supported'
         return None, _filt, _inst, _det, msg
-
-    if _filt not in ['F150W','F200W','F182M','F210M','F140M',]:
+        
+    # F150W2 is F160M for now
+    if _filt not in ['F150W','F200W','F182M','F210M','F140M','F150W2']:
         msg = f'NIRCam filter {_filt} not supported'
         return None, _filt, _inst, _det, msg
 
